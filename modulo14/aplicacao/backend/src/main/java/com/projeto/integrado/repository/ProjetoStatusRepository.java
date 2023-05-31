621049d6da9741d6f433f272cc145a046ab7ef70
luizphp12 <72347798+luizphp12@users.noreply.github.com>
2023-05-31 16:17:54 -0300

    Update ProjetoStatusRepository.java

--- modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/ProjetoStatusRepository.java
+++ modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/ProjetoStatusRepository.java
@@ -3,7 +3,8 @@ package com.projeto.integrado.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.projeto.integrado.entity.Projeto;
+import com.projeto.integrado.entity.ProjetoStatus;
 
-public interface ProjetoStatusRepository extends JpaRepository<User, Integer> {
-	Optional<Status> findByProjetoStatus(User ProjetoStatus);
+public interface ProjetoStatusRepository extends JpaRepository<ProjetoStatus, Integer> {
+	Optional<Status> findByProjetoStatus(String projetoStatus);
 }
