7ae58da5aef88098b0c52d7fc31bd4095b0d8028
luizphp12 <72347798+luizphp12@users.noreply.github.com>
2023-05-31 16:12:34 -0300

    Update EmailRepository.java

--- modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/EmailRepository.java
+++ modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/EmailRepository.java
@@ -6,5 +6,5 @@ import com.projeto.integrado.entity.UserEmail;
 import com.projeto.integrado.entity.User;
 
 public interface EmailRepository extends JpaRepository<User, Integer> {
-	Optional<Tarefa> findByEmail(User UserEmail);
+	Optional<Email> findByEmail(User UserEmail);
 }
