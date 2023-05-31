eee28e78ed5d00d8d54ddbfe4e6458bf56cb8b2c
luizphp12 <72347798+luizphp12@users.noreply.github.com>
2023-05-31 16:12:14 -0300

    Update and rename NomeRepository,java to NomeRepository.java

--- modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/NomeRepository.java
+++ modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/NomeRepository.java
@@ -2,9 +2,9 @@ package com.projeto.integrado.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.projeto.integrado.entity.UserEmail;
+import com.projeto.integrado.entity.UsuarioNome;
 import com.projeto.integrado.entity.User;
 
-public interface EmailRepository extends JpaRepository<User, Integer> {
-	Optional<Tarefa> findByEmail(User UserEmail);
+public interface NomeRepository extends JpaRepository<User, Integer> {
+	Optional<Nome> findByNome(User UsuarioNome);
 }
