8f8dd23545dea17c80a54be746b6d4e488399a24
luizphp12 <72347798+luizphp12@users.noreply.github.com>
2023-05-31 15:59:05 -0300

    Update header.js

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,31 +5,38 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+import AddIcon from '@mui/icons-material/Add';
+
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+return (
+<Box sx={{ flexGrow: 1 }}>
+<AppBar position="static">
+<Toolbar>
+<IconButton
+size="large"
+edge="start"
+color="inherit"
+aria-label="menu"
+sx={{ mr: 2 }}
+>
+{/*
+<MenuIcon />
+*/}
+</IconButton>
+<Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+Lousa de Atividades
+</Typography>
+<Button
+variant="contained"
+color="secondary"
+startIcon={<AddIcon />}
+>
+Adicionar Tarefa
+</Button>
+</Toolbar>
+</AppBar>
+</Box>
+);
 }
- 
+
 export default Header;
